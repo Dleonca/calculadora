f6d7d7d0beb206333b5ed98394965da398cddbe4
Daniela Leon Cantero <62155571+Dleonca@users.noreply.github.com>
2023-03-30 09:53:02 -0500

    commit

---
+++ js/calculator.js
@@ -0,0 +1,8 @@
+
+function operaciones(num1, num2, operacion){
+    if (operacion === 'suma'){
+       Resultado = num1 + num2
+    }else if(operacion === 'resta'){
+        Resultado = num1 + num2
+    }
+}
